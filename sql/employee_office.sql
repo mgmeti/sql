ab9c8c7b9766699695b233baf07c18cba8967277
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-02-12 14:02:30 +0530

    code to create initial tables
    employee office data model with table creating sql code

---
+++ sql/employee_office.sql
@@ -0,0 +1,35 @@
+create table department(
+dept_id number(2) primary key,
+name varchar2(25)
+);
+
+
+ create table insurance(
+ insur_id number(5) primary key,
+ name varchar2(25)
+ );
+
+
+create table employee(
+emp_id number(3) primary key,
+name varchar2(25),
+age number(2),
+gender char(1),
+dept_id number(2),
+insur_id number(5),
+foreign key(dept_id) references department(dept_id),
+foreign key(insur_id) references insurance(insur_id)
+);
+
+create table office(
+id number(3) primary key,
+name varchar2(25)
+);
+
+create table employee_office(
+id number(3) primary key,
+emp_id number(3),
+office_id number(3),
+foreign key(emp_id) references employee(emp_id),
+foreign key(office_id) references office(id)
+);
