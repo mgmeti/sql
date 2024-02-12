8655df018408c14bfb0057ddca2fe612713daa21
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-02-12 14:11:17 +0530

    comments added with author details

--- sql/employee_office.sql
+++ sql/employee_office.sql
@@ -1,15 +1,26 @@
+--Author: Arjun
+--Date: 12-02-2024
+
+
+--Independent tables -> The tables which does not have FK 
+
 create table department(
 dept_id number(2) primary key,
 name varchar2(25)
 );
-
 
  create table insurance(
  insur_id number(5) primary key,
  name varchar2(25)
  );
 
+create table office(
+id number(3) primary key,
+name varchar2(25)
+);
 
+
+--Dependent tables -> The tables does have FK
 create table employee(
 emp_id number(3) primary key,
 name varchar2(25),
@@ -19,11 +30,6 @@ dept_id number(2),
 insur_id number(5),
 foreign key(dept_id) references department(dept_id),
 foreign key(insur_id) references insurance(insur_id)
-);
-
-create table office(
-id number(3) primary key,
-name varchar2(25)
 );
 
 create table employee_office(
