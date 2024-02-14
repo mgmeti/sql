8de6de7e5459e3f8a9aaee0480eb0006f9c46283
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-02-14 14:05:46 +0530

    Added create ddl with constraint refereces

---
+++ sql/table_constraint_references.sql
@@ -0,0 +1,19 @@
+--create table with constraints with references
+create table doctor(
+id number(3),
+name varchar2(25),
+spec varchar2(15),
+constraint pk_id  primary key(id),
+constraint uniq_spec unique(spec)
+);
+
+
+create table patient(
+id number(5),
+name varchar2(25) not null,
+disease varchar2(30),
+did number(3),
+constraint pid_pk primary key(id),
+constraint did_fk foreign key(did) references doctor(id)
+);
+
