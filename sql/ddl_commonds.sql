4a45983c65b3edf8fbd2439a88c3a8458a151131
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-02-15 13:40:28 +0530

    DDL alter detials are added

--- sql/ddl_commonds.sql
+++ sql/ddl_commonds.sql
@@ -37,3 +37,35 @@ rename old_table_name to new_table_name;
 --TO copy columns and records from one table to new table.
 
 create table new_table_name as select * from old_table_name; 
+
+--NOTE: 1) To store the data in a specific new column or at specific cell, we need to use 	update.
+--	2) insert always creates a new record in table.
+
+--ALTER
+--Add a column, 
+--alter table table_name add col_name datatype;
+alter table emp1 add email varchar2(25);
+
+--drop a column
+--alter table table_name drop column col_name;
+alter table emp1 drop column comm;
+
+--rename a column
+--alter table table_name rename column old_col_name to new_col_name;
+alter table emp1 rename column hiredate to joindate;
+-- rename  a table
+--alter table table_name raname to new_table_name;
+alter table emp1 rename to emp2;
+
+-- change the datatype with contraints
+--alter table table_name modify column_name new_datatype;
+alter table emp2 modify email varchar2(20);
+
+--NOTE: 
+--	Columns needs to be empty while changing to new datatype of column
+--	We can increase datatype size or derease upto maximum size of stored data in column
+
+--drop contraints
+
+
+-- add  FK's
