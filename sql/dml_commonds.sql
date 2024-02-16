26faf2b3ef0c4d03dbd92b9ea2b29ee02518088e
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-02-16 13:58:40 +0530

    Second version of insert into statement with giving column names

--- sql/dml_commonds.sql
+++ sql/dml_commonds.sql
@@ -1,12 +1,19 @@
 --DML[insert, delete, update]
 --Data manipulation language
---These commonds are not auto-committed(no permanent chnage until we use TCL commonds)
+--These commands are not auto-committed(no permanent change until we use TCL commands)
 --to save changes to db, we need to call commit.
 -- select ... into -> dql -it is part of DML.
 
 --INSERT
---insert values tot the table
+--insert values to the table
 --insert into table_name[optional-column names] values ((one record-value1,value2..),(second record-value1, value2...));
 
 insert into ord values(5, null);
 insert into ord values(4, 'ORDER RECEIVED');
+
+
+--It is used to insert values to specific columns.
+insert into cust(cno, ono, name) values(6, 5, 'arjun');
+
+--It will include null values for other columns.
+insert into cust(cno) values(8); 
