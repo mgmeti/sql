c250ba21617f7d4822218fc5dd851fba3c4a5fe2
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-02-21 10:17:55 +0530

    Datatypes basics

---
+++ sql/sql_datatypes_oracle.sql
@@ -0,0 +1,20 @@
+--Datatypes
+--It is applied at column level
+--Specified while creating table
+--char, varchar2(), number(p, s), date, timestamp, clob, blob
+
+--char -> FIXED MEMORY, Stores alphanumeric values, 'value' -> single quotes are used
+--varchar2 -> FLEXIBLE MEMORY, stores alphanumeric values, 'value' -> single quotes are used
+--number(p, s) -> p-precision, s-scale
+        --> number(3, 0) -->-999, 999
+        -->number(3, 1) --> -99.9, 99.9
+        -->number(3, -1) --> 990, -990
+
+create table data_type(
+    date_col date,
+    num_col number(3),
+    varchar_col varchar2(25),
+    timestamp_col timestamp,
+    blob_col blob,
+    clob_col clob,
+)
