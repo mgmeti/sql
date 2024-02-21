f5504686d7c5340a2f6c6246e35dbcbf3c9bf496
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-02-21 09:37:29 +0530

    Basics about sqlplus software

---
+++ sql/sql_plus_oracle_databse_notes.sql
@@ -0,0 +1,35 @@
+--Athor: ARJUN
+--date: 21-02-2022
+
+
+--Keywords used to work with sqlPlus software
+--SHOW:
+--It is used to show curent user, tab, pages and lines size
+show tab;
+show user;
+show pages;
+show lines;
+
+--SET:
+--It is used to set pages and & lines size
+--It is not permanent change, and changes are session specific.
+set pages 200 lines 200;
+
+--CL SCR:
+--It is used to clear current screen
+cl scr;
+
+--DESC/DESCRIBE:
+--It is used to decribe the a table
+--It will give details like column names, about null contraint, datatypes with sizes
+desc table_name;
+decribe table_name;
+
+--ED:
+--It used to edit previous sql statement in notepad
+--After changes made, save the file, and close the notepad.
+-- Enter '/' to run statement.
+ed;
+
+--CONNECT:
+--It is used to connect to different user in the database by entering username and password.
