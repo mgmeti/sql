e653e3fd0dc0570ce5b99f9ad1ed2c73d6a36ecd
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-02-22 13:55:17 +0530

    TCL commonds basics

---
+++ sql/tcl_commonds.sql
@@ -0,0 +1,12 @@
+--TCL --> Transaction Control Language [COMMIT, ROLLBACK, SAVEPOINT]
+--TCL are applied to only for DML trasactions like insert, updaste, and delete
+--TCL commonds are used to control transaction done by DML commonds.
+--NOTE: Transaction: one unit of work is done by using one or more sql commonds
+
+--COMMIT --> Saves the data permanently into the database.
+-- Syntax: commit;
+commit;
+
+--ROLLBACK --> used to undo the transaction till last commit.
+-- syntax: rollback;
+rollback;
