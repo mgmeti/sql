a10690179508bef0662e0e98d545d3e9e4137586
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-02-28 17:51:31 +0530

    Questions related to logical operators

--- sql/dcl_commonds.sql
+++ sql/dcl_commonds.sql
@@ -81,8 +81,42 @@ from emp
 where job = 'MANAGER' and sal >2500;
 
 -- Relational operators
-select ename, job, sal
+--AND--> used to combine multiple conditions in where clause
+
+--Q1
+select ename, sal, deptno
+from emp
+where sal > 2000 and deptno = 10;
+
+--Q2
+select *
+from emp
+where empno = 7902 and job='SALESMAN';
+
+--Q3
+select ename, mgr, comm
+from emp
+where mgr = 7839 and comm > 300;
+
+--Q4
+select ename, hiredate, deptno
+from emp
+where deptno = 20 and hiredate = '03-DEC-81';
+
+--Q5
+select ename, job, deptno 
+from emp
+where deptno = 20 and job = 'ANALYST';
+
+--Q6
+select *
 from emp
 where sal > 1000 and sal < 3000 and job='ANALYST';
+
+--Q7
+select ename
+from emp
+where empno=7788 and job<>'MANAGER' and job<>'CLERK';
+
 
 --ORDER OF EXECUTION --> FROM -> WHERE -> SELECT ( as of now)
