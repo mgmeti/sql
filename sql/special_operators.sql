962636540f59f4dfbf3cb5118e19e75d35c07ed8
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-03-01 16:37:49 +0530

    IS not null and some basics examples.

--- sql/special_operators.sql
+++ sql/special_operators.sql
@@ -37,27 +37,56 @@ where job!='CLERK' and not job='SALESMAN' and comm is null;
 
 
 --IS NOT NULL
+--Used to evaluate NOT NULL values inside the specific column.
 --Syntax--> column_name IS NOT NULL;
+
 --Works in some department
  select *
  from emp
  where deptno is not null;
-
 
 --all employee who get some salary
 select *
 from emp
 where sal is not null;
 
---Employee's who have manager
-select *
-from emp
-where mgr is not null;
-
 --Employee's who do not get commision and works iin department and gets salary
 select *
 from emp
 where deptno is not null and sal is not null and comm is null;
+
+--Q1
+select *
+from emp
+where sal is not null and deptno=10;
+--3 rows selected.
+
+--Q2
+--Employee's who have manager
+select *
+from emp
+where mgr is not null;
+--13 rows selected.
+
+--Q3
+select *
+from emp
+where comm is not null;
+--4 rows selected.
+
+--Q4
+select ename, sal, deptno
+from emp
+where sal is not null and deptno is not null;
+--14 rows selected.
+
+--Q5
+select *
+from emp
+where comm is null  and deptno is not null and mgr is not null;
+--9 rows selected.
+
+
 
 
 
