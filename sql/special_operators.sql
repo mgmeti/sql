f3d96a413fe042885ca2cb8ed47939ddc01dff01
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-03-01 16:19:58 +0530

    Is not null operator with examples.

--- sql/special_operators.sql
+++ sql/special_operators.sql
@@ -1,4 +1,43 @@
 --Special Operators
 --IS
 --It id used to evaluate null values
---Syntax--> column_name is null;
+--Syntax--> column_name IS NULL;
+
+--Employee who does not have manager assigned
+select * 
+from emp
+where mgr is null;
+
+--Employee who does not get commision 
+select * 
+from emp
+where comm is null;
+
+--Employee who does not get commision and not assigned manager
+select * 
+from emp
+where comm is null and mgr is null;
+
+--IS NOT NULL
+--Syntax--> column_name IS NOT NULL;
+--Works in some department
+ select *
+ from emp
+ where deptno is not null;
+
+
+--all employee who get some salary
+select *
+from emp
+where sal is not null;
+
+--Employee's who have manager
+select *
+from emp
+where mgr is not null;
+
+select *
+from emp
+where deptno is not null and sal is not null and comm is null;
+
+
