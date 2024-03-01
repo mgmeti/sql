7c740512eec0158013f8ab52a1d1d9b5c73ffa3f
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-03-01 18:04:58 +0530

    Added context to the SQL statements.

--- sql/In_operator.sql
+++ sql/In_operator.sql
@@ -26,6 +26,7 @@ where sal > 2000 and sal < 5000 and job in ('ANALYST', 'MANAGER') and deptno in 
 --1 rows selected.
 
 --Q4
+--Employee's who is working in deprtment 10 or 20 or 30 or 40 or 50.
 select ename, deptno
 from emp
 where deptno in (10, 20, 30, 40, 50);
@@ -41,14 +42,30 @@ select ename, job, sal, deptno
 from emp
 where deptno in (10, 20) and empno not in (3, 4);
 
+--Q1
+--Employee's who is not in department 10 and 20.
+select *
+from emp
+where deptno not in (10, 20);
+--6 rows selected.
+
+--Q2
+--Employee who is not working as analyst, manager, clerk, and salesman
+select *
+from emp
+where job not in ('ANALYST', 'CLERK', 'MANAGER', 'SALESMAN');
+--1 rows selected.
+
 
 --Q3
+--Employee's except given jobs clerk and salesman, also except given department 10 and 30.
 select ename, sal, deptno, job
 from emp
 where job not in ('CLERK', 'SALESMAN') and deptno not in (10, 30);
 --3 rows selected.
 
 --Q4
+--Employee's who are not earning any commission but works in some department as clerk or salesman, excpet in daprtment 20 and 30.
 select *
 from emp
 where comm is null and deptno is not null and job in ('CLERK', 'SALESMAN') and deptno not in (20, 30);
