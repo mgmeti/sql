e3634c83d2ed02181dfb59f66e693c722f64d405
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-03-04 18:13:25 +0530

    Not like operator.

---
+++ sql/Not_likeoperator.sql
@@ -0,0 +1,27 @@
+--Not like operator
+-- It is just opposite to like operator.
+-- It rejects the given pattern
+--Syntax: column_name not like 'pattern';
+
+--Names who does not starts with 'S'.
+select ename
+from emp
+where ename not like 'S%';
+--12 rows selected.
+
+select ename
+from emp
+where ename not like 'A%' and  ename not like 'S%';
+--10 rows selected.
+
+--Names starts with consonants only.
+select ename
+from emp
+where ename not like 'A%' and  ename not like 'E%' and ename not like 'I%' and ename not like 'O%' and  ename not like 'U%' ;
+--12 rows selected.
+
+--Names ends with consonants only.
+select ename
+from emp
+where ename not like '%A' and ename not like '%E' and ename not like '%I' and ename not like '%O' and ename not like '%U' ;
+--13 rows selected.
