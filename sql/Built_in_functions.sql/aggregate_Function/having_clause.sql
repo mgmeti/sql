1c2d90adc38dba01088e35d89b34e54173ff28ed
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-03-23 17:57:52 +0530

    Eamples on group by and having clauses.

--- sql/Built_in_functions.sql/aggregate_Function/having_clause.sql
+++ sql/Built_in_functions.sql/aggregate_Function/having_clause.sql
@@ -54,6 +54,144 @@ group by sal  -- 2
 having count(sal) >= 2;  -- 3
 
 
+--Display the names that is repeated.
+select ename, count(ename)  
+from emp    
+group by ename  
+having count(ename) >= 2;  
+
+--Display department, salary where the employee earnig same salary in same department.
+select deptno, sal
+from emp
+group by sal, deptno
+having count(*) > 1;
+
+-- Job's which has atleast 3 employees
+select job, count(ename)"# employees"
+from emp
+group by job
+having count(ename) >= 3;
+
+--Departments which has atleast 5 employees
+select deptno, count(ename)"# employees"
+from emp
+group by deptno
+having count(ename) >= 5;
+
+--Display department if it has atleast 2 manager's.
+select deptno
+from emp
+where job = 'MANAGER'
+group by deptno
+having count(job) >= 2;
+
+select max(sal), min(sal), avg(sal)
+from emp
+group by deptno
+having min(sal) < avg(sal);
+
+select max(sal), min(sal), avg(sal)
+from emp
+group by deptno
+having min(sal) < avg(sal);
+
+select max(ename), min(ename), count(*)
+from emp
+group by deptno
+having count(*)>1;
+
+--Q1
+select deptno, count(ename)
+from emp
+group by deptno
+having count(ename) >=2;
+
+--Q2
+select job, count(ename)
+from emp
+group by job
+having count(ename) >=3;
+
+--Q3
+--Display the salary that is repeated.
+select sal, count(sal)  
+from emp    
+group by sal  
+having count(sal) >= 2;  
+
+--Q4
+--Display the names that is repeated.
+select ename, count(ename)  
+from emp    
+group by ename  
+having count(ename) >= 2; 
+
+--Q5
+select deptno, count(ename)
+from emp
+where job = 'CLERK'
+group by deptno
+having count(job) >=2;
+
+--Q6
+select deptno, sum(sal)
+from emp
+group by deptno
+having count(ename) >= 4;
+
+--Q7
+select job, count(ename), sum(sal)
+from emp
+where sal > 1200
+group by job
+having sum(sal) >3800 ;
+
+--Q8
+select deptno, count(ename)
+from emp
+where job = 'ANALYST'
+group by deptno
+having count(ename) >=2;
+
+--Q9
+select job, min(sal), max(sal)
+from emp
+group by job
+having min(sal) > 1000 and max(sal) < 5000;
+
+--Q10
+select deptno, avg(sal)
+from emp
+group by deptno
+having avg(sal) > 3000 and max(sal) > 2600;
+
+
+--Q11
+select hiredate
+from emp
+group by hiredate
+having count(hiredate) >= 2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
