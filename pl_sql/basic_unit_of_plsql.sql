3f3235a0d724e52fcb55f93f6fbdc36d28f76bdd
mgmeti <89732992+mgmeti@users.noreply.github.com>
2024-03-25 16:44:59 +0530

    Basic examples about pl sql.

--- pl_sql/basic_unit_of_plsql.sql
+++ pl_sql/basic_unit_of_plsql.sql
@@ -3,3 +3,32 @@
         -- DECLARATION BLOCK (OPTIONAL)
         -- EXECUTION BLOCK (MUST)
         -- EXCEPTION HANDLING BLOCK (OPTIONAL)
+
+--------------------------------
+declare
+    v_name varchar2(25);
+    v_number number;
+begin 
+        v_name := 'arjun';
+        v_number := 1000;
+
+        dbms_output.put_line('Name '|| v_name);
+        dbms_output.put_line('number '|| v_number);
+end;
+/
+----------------------------------------
+
+--Select into..
+declare
+        name varchar2(20);
+        salary number(8);
+begin
+        select ename, sal into name, salary
+        from emp
+        where empno = 7686;
+        dbms_output.put_line('Name '|| name || ' and salary '||salary);
+end;
+/
+
+----------------------------------------------
+
